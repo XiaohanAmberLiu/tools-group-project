829c71a388d6a747d87bbe03ab7a142d9ad7f95b
Zhangms33 <44420507+Zhangms33@users.noreply.github.com>
2018-11-29 22:39:20 -0500

    Update Selenium for Airbnb

--- Get_Hotel_Info_selenium.py
+++ Get_Hotel_Info_selenium.py
@@ -1,145 +1,93 @@
 
 # coding: utf-8
 
-# In[ ]:
-
-#!pip install selenium
-
-
-# In[2]:
+# In[1]:
 
 def get_main_url(destination,adult_num,child_num,cktin_date,cktout_date):
     url="https://www.airbnb.com/s/"+destination+"/homes?adults="+adult_num+"&children="+child_num+"&checkin="+cktin_date+"&checkout="+cktout_date+"&refinement_paths%5B%5D=%2"+"Fhomes&allow_override%5B%5D=&s_tag=Vobyce0e"
     return url
 
 
-# In[3]:
+# In[2]:
 
 url=get_main_url('Paris--France','1','0','2019-02-14','2019-02-21')
 url
 
 
-# In[4]:
+# In[12]:
 
-def get_onepage_info(url):
+def get_onepage_info(url): 
     from selenium import webdriver
     from selenium.webdriver.common.keys import Keys
     from selenium.webdriver.chrome.options import Options
     import re
     import pandas as pd
 
-
     chrome_options = Options()
     chrome_options.add_argument('--headless')
     driver = webdriver.Chrome('/Users/zhangmingshan/Desktop/2018 Autumn/Tools for Analytics/Project/chromedriver')
-    driver.get(url)
+    try:
+        driver.get(url)
+    except:
+        return "Connection Failure"
+
     driver.implicitly_wait(10)
-    #find all names
-    names_list=list()
-    names=driver.find_elements_by_class_name('_2izxxhr')
-    for name in names:
-        names_list.append(name.text)
+    Hotels=driver.find_elements_by_class_name('_qlq27g')
+    result=list()
+    for Hotel in Hotels:
+        #get hotel url
+        try:
+            link=Hotel.find_elements_by_tag_name('a')
+            l=link[0].get_attribute('href')
+        except:
+            l=""
 
-    #find all prices
-    prices_list=list()
-    prices=driver.find_elements_by_class_name('_p1g77r')
-    for price in prices: 
-        p=re.search(r'\d+',price.text)
-        prices_list.append(p.group())
+        #get hotel name
+        try:
+            name=Hotel.find_elements_by_class_name('_2izxxhr')
+            n=name[0].text
+        except:
+            n=""
 
+        #get hotel description
+        try:
+            Descrips=Hotel.find_elements_by_class_name('_1nhodd4u')
+            d=""
+            for Descrip in Descrips:
+                d += ' · ' + Descrip.text
+        except:
+            d=""
 
-    #find all descriptions
-    descriptions_list=list()
-    descriptions=driver.find_elements_by_class_name('_1nhodd4u')
-    for description in descriptions:
-        descriptions_list.append(description.text)
+         #get hotel price in total
+        try:
+            Price=Hotel.find_elements_by_class_name('_p1g77r')
+            p=re.search(r'\d+',Price[0].text).group()
+        except:
+            p=""
 
-    #find urls
-    hotel_urls=list()
-    links=driver.find_elements_by_xpath("//a[contains(@href,'rooms')]")
-    for link in links:
-        hotel_urls.append(link.get_attribute('href'))
-        
-    #hotel_data = pd.DataFrame({'Name': names_list,'Prices_total': prices_list,'Description': descriptions_list,'Urls':hotel_urls})
-    
-    return (names_list,prices_list,descriptions_list,hotel_urls)
+        #rating
+        try:
+            rating=Hotel.find_elements_by_class_name('_q27mtmr')
+            ra=rating[0].find_elements_by_tag_name('span')
+            r=ra[0].get_attribute('aria-label')
+        except:
+            r=""
 
+        ##num of reviews
+        try:
+            review=Hotels[0].find_elements_by_class_name('_1m8bb6v')
+            for x in review:
+                temp=re.match(r'\d+',x.text)
+                if temp:
+                    rev=temp.group()
+        except:
+            rev=""
 
-# In[5]:
-
-a=get_onepage_info(url)
-
-
-# In[6]:
-
-a
-
-
-# In[7]:
-
-print(len(a[0]),len(a[1]),len(a[2]),len(a[3]))
-
-
-# In[ ]:
-
-for _ in range(100):
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-# print all of the page source that was loaded
-print(driver.page_source.encode("utf-8"))
-
-
-# In[ ]:
-
-#Output Hotel: (name, address, zip code, rating, room_type, beds, people, price)
-names=driver.find_elements_by_class_name('_2izxxhr')
-for name in names:
-    print(name.text)
+        result.append((n,p,r,rev,d,l))
+    return result
 
 
-# In[ ]:
+# In[13]:
 
-import re
-a='$123jiom'
-match=re.search(r'^[$]',a)
-bool(match)
-
-
-# In[ ]:
-
-import re
-prices=driver.find_elements_by_class_name('_p1g77r')
-for price in prices:
-    print(re.search(r'\d+',price.text).group())
-
-
-# In[ ]:
-
-descriptions=driver.find_elements_by_class_name('_1nhodd4u')
-for description in descriptions:
-    print(description.text)
-
-
-# In[ ]:
-
-driver.refresh()
-links=driver.find_elements_by_xpath("//a[contains(@href,'rooms')]")
-hotel_urls=list()
-for link in links:
-    hotel_urls.append(link.get_attribute('href'))
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-hotel_urls
-
-
-# In[ ]:
-
-###inside each url
+get_onepage_info(url)
 
