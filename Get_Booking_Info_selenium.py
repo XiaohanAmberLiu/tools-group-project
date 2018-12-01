4b130131507a16a2bdcb4438048143a15841487a
Zhangms33 <44420507+Zhangms33@users.noreply.github.com>
2018-11-30 23:57:02 -0500

    Almost finish but some bug still

--- Get_Booking_Info_selenium.py
+++ Get_Booking_Info_selenium.py
@@ -1,13 +1,14 @@
 
 # coding: utf-8
 
-# In[11]:
+# In[6]:
 
 #send basic info to booking
 def set_basic_info(destination,adult_num,child_num,cktin_date,cktout_date):
     from selenium import webdriver
     from selenium.webdriver.common.keys import Keys
     from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.support.ui import Select
     import re
     import time
     import re
@@ -22,7 +23,7 @@ def set_basic_info(destination,adult_num,child_num,cktin_date,cktout_date):
     except:
         print("Connection Failure")
     driver.maximize_window()
-    driver.implicitly_wait(5)
+    driver.implicitly_wait(15)
     
     #Set destination
     dest=driver.find_element_by_xpath("//input[@type='search']")
@@ -65,7 +66,7 @@ def set_basic_info(destination,adult_num,child_num,cktin_date,cktout_date):
     person_button=driver.find_element_by_xpath("//label[@for='xp__guests__input']")
     person_button.click()
 
-    #adult
+    #set people
     #person+button
     add_button=driver.find_elements_by_xpath("//button[@type='button'][@class='bui-button bui-button--secondary bui-stepper__add-button']")
     add_button=add_button[1:]
@@ -81,20 +82,29 @@ def set_basic_info(destination,adult_num,child_num,cktin_date,cktout_date):
         child_web=int(re.findall('\d+',guests)[1])
         adult_num=int(adult_num)
         child_num=int(child_num)
-
+    
         if (adult_web!=adult_num) or (child_web!=child_num):
-            if adult_web>adult_num:
-                sub_button[0].click()
-            elif adult_web<adult_num:
-                add_button[0].click()
+            try:
+                if adult_web>adult_num:
+                    sub_button[0].click()
+                elif adult_web<adult_num:
+                    add_button[0].click()
 
-            if child_web>child_num:
-                sub_button[1].click()
-            elif child_web<child_num:
-                add_button[1].click()
+                if child_web>child_num:
+                    sub_button[1].click()
+                elif child_web<child_num:
+                    add_button[1].click()
+            except:
+                select_a = Select(driver.find_element_by_name('group_adults'))
+                select_a.select_by_value(str(adult_num))
+                select_c = Select(driver.find_element_by_name('group_children'))
+                select_c.select_by_value(str(child_num))
+                break
+                
+                
         else:
             break
-       
+    
     #click submit
     submit=driver.find_element_by_xpath("//button[@type='submit'][@data-sb-id='main']")
     submit.click()
@@ -102,7 +112,7 @@ def set_basic_info(destination,adult_num,child_num,cktin_date,cktout_date):
     return driver
 
 
-# In[12]:
+# In[7]:
 
 #destination,adult_num,child_num,cktin_date,cktout_date,budget
 cktin_date='2019-02-14'
@@ -110,10 +120,89 @@ cktout_date='2019-02-21'
 adult_num=1
 child_num=0
 destination='Paris'
-set_basic_info(destination,adult_num,child_num,cktin_date,cktout_date)
+driver=set_basic_info(destination,adult_num,child_num,cktin_date,cktout_date)
 
 
-# In[ ]:
+# In[5]:
+
+#def get_page_info(url,budget,page_total_num=1): 
+
+budget=1000
+page_total_num=1
 
 
+page=1
+result=list()
+while page<=page_total_num:
+    driver.implicitly_wait(10)
+    Hotels=driver.find_elements_by_xpath("//div[@class='sr_item_content sr_item_content_slider_wrapper ']")
+    for Hotel in Hotels:
+        #get hotel price in total
+        try:
+            Price=Hotel.find_elements_by_tag_name('strong')
+            pr=Price[0].text
+
+            #delete the ',' in price
+            pr=pr.replace(',','')
+            p=int(re.search(r'\d+',pr).group())
+            print(p)
+
+#             if p<=budget:
+#                 #get hotel url
+#                 try:
+#                     link=Hotel.find_elements_by_xpath("//a[@class='hotel_name_link url']")
+#                     l=link[0].get_attribute('href')
+#                 except:
+#                     l=""
+
+#                 #get hotel name
+#                 try:
+#                     name=Hotel.find_elements_by_class_name('sr-hotel__name')
+#                     n=name[0].text
+#                 except:
+#                     n=""
+
+#                 #get hotel description
+#                 try:
+#                     Descrips=Hotel.find_elements_by_class_name('room_link')
+#                     d=Descrips[0].text
+#                     d=d.replace('\n',' · ')
+#                 except:
+#                     d=""
+
+#                 #rating
+#                 try:
+#                     rating=Hotel.find_elements_by_class_name('bui-review-score__badge')
+#                     r=rating[0].text
+#                 except:
+#                     r=""
+
+#                 ##num of reviews
+#                 try:
+#                     review=Hotel.find_elements_by_class_name('bui-review-score__text')
+#                     rev=re.match(r'\d+',review[0].text.replace(',','')).group()
+
+#                 except:
+#                     rev=""
+                    
+#                 result.append((n,p,r,rev,d,l))
+#             else:
+#                 continue
+
+        except:
+            continue
+
+    page += 1  
+    driver.find_element_by_xpath("//li[@class='bui-pagination__item bui-pagination__next-arrow']").click()
+    driver.refresh()
+
+# #driver.close()
+# result
+#return result
+
+
+
+# In[4]:
+
+driver.close()
 
