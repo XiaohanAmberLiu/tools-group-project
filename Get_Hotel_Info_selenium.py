2e98f76a14e8de56c9c679e675c6010567398875
Zhangms33 <44420507+Zhangms33@users.noreply.github.com>
2018-11-30 22:47:29 -0500

    A little correction on number of reviews

--- Get_Hotel_Info_selenium.py
+++ Get_Hotel_Info_selenium.py
@@ -1,14 +1,14 @@
 
 # coding: utf-8
 
-# In[ ]:
+# In[8]:
 
 def get_main_url(destination,adult_num,child_num,cktin_date,cktout_date):
     url="https://www.airbnb.com/s/"+destination+"/homes?adults="+adult_num+"&children="+child_num+"&checkin="+cktin_date+"&checkout="+cktout_date+"&refinement_paths%5B%5D=%2"+"Fhomes&allow_override%5B%5D=&s_tag=Vobyce0e"
     return url
 
 
-# In[ ]:
+# In[12]:
 
 def get_page_info(url,budget,page_total_num=1): 
     from selenium import webdriver
@@ -24,7 +24,8 @@ def get_page_info(url,budget,page_total_num=1):
         driver.get(url)
     except:
         return "Connection Failure"
-
+    driver.maximize_window()
+    
     page=1
     result=list()
     while page<=page_total_num:
@@ -74,11 +75,12 @@ def get_page_info(url,budget,page_total_num=1):
 
                     ##num of reviews
                     try:
-                        review=Hotels[0].find_elements_by_class_name('_1m8bb6v')
+                        review=Hotel.find_elements_by_class_name('_1m8bb6v')
                         for x in review:
                             temp=re.match(r'\d+',x.text)
                             if temp:
                                 rev=temp.group()
+
                     except:
                         rev=""
 
@@ -92,21 +94,22 @@ def get_page_info(url,budget,page_total_num=1):
         page += 1  
         driver.find_element_by_class_name('_1rltvky').click()
         driver.refresh()
-                                
+    
+    driver.close()
     return result
 
 
-# In[ ]:
+# In[13]:
 
 def get_airbnb_list(destination,adult_num,child_num,cktin_date,cktout_date,budget):
     url=get_main_url(destination,adult_num,child_num,cktin_date,cktout_date)
     return get_page_info(url,budget,page_total_num=1)
 
 
-# In[ ]:
+# In[14]:
 
 #example
-get_airbnb_list('New York','1','0','2019-02-14','2019-02-21',600)
+get_airbnb_list('Paris','1','0','2019-02-14','2019-02-21',600)
 
 
 # In[ ]:
