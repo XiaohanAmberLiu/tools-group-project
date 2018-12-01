51d8daa42c728015432e1a2fb77f4d1b8a4f40c5
laSeinelaSeine <44626166+laSeinelaSeine@users.noreply.github.com>
2018-12-01 02:42:20 -0500

    Hotels.com selenium web crawling
    still have bugs crawling price

--- Hotels.com.py
+++ Hotels.com.py
@@ -4,7 +4,13 @@
 # In[39]:
 
 
-url = 'https://www.hotels.com/search.do?resolved-location=CITY%3A504261%3AUNKNOWN%3AUNKNOWN&destination-id=504261&q-destination=Paris,%20France&q-check-in=2019-02-14&q-check-out=2019-02-21&q-rooms=1&q-room-0-adults=2&q-room-0-children=0'
+city = 'Paris'
+cktin = '2019-02-14'
+cktout = '2019-02-21'
+rooms = '1'
+adults = '2'
+children = '0'
+url of hotel.com = "https://www.hotels.com/search.do?3A504261%3AUNKNOWN%3AUNKNOWN&q-destination="+city+"&q-check-in="+cktin+"&q-check-out="+cktout+"&q-rooms="+rooms+"&q-room-0-adults="+adults+"&q-room-0-children="+children+""
 url
 
 
@@ -14,7 +20,8 @@ url
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-
+import re
+import pandas as pd
 
 # In[41]:
 
@@ -27,44 +34,33 @@ driver.implicitly_wait(10)
 
 
 # In[42]:
+hotel_list = []
+hotels = driver.find_elements_by_class_name('hotel')
+hotels_sponsored = driver.find_elemnets_by_class_name('hotel.sponsored')
+hotels_vip = driver.find_elements_by_class_name('hotel.vip')
+for hotel in hotels:
+    name = hotel.find_elements_by_class_name('p-name')[0].text
+    address = hotel.find_elements_by_class_name('p-adr')[0].text
+    #price = hotel.find_elements_by_tag_name('ins')[0].text, stil have some bugs
+    review = hotel.find_elements_by_class_name('guest-reviews')[0].text
+    link = hotel.find_elements_by_tag_name('a')[0].get_attribute('href')
+    hotel_list.append((name,address,review,link,#price))
 
-
+#old version codes as follows
+                       
 names = driver.find_elements_by_class_name('p-name')
 for name in names:
     print(name.text)
-
-
-# In[43]:
-
 
 addresses = driver.find_elements_by_class_name('p-adr')
 for address in addresses:
     print(address.text)
 
-
-# In[46]:
-
-
 prices = driver.find_elements_by_class_name('price')
 for price in prices:
     print(price.text)
 
-
-# In[47]:
-
-
 reviews = driver.find_elements_by_class_name('guest-reviews')
 for review in reviews:
     print(review.text)
-
-
-# In[48]:
-
-
-driver.refresh()
-links=driver.find_elements_by_xpath("//a[contains(@href,'flex-link')]")
-hotel_urls=list()
-for link in links:
-    hotel_urls.append(link.get_attribute('href'))
-print(hotel_urls)
 
