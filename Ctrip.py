922a8551e46ae5d687350d6642ca887ceb24e096
laSeinelaSeine <44626166+laSeinelaSeine@users.noreply.github.com>
2018-12-01 02:44:28 -0500

    Update Ctrip.py
    url cannot be generated by function, would give up Ctrip

--- Ctrip.py
+++ Ctrip.py
@@ -1,40 +1,4 @@
 
-# coding: utf-8
-
-# In[49]:
-
-
-url = 'https://www.trip.com/hotels/list?city=192&checkin=02-14-2019&checkout=02-21-2019&optionId=192&optionType=Intlcity&display=Paris%2C%20Ile-De-France%2C%20France&adult=1&children=0&ages=&label=LzdP1uB0N06XFxqjKHHdVg#ctm_ref=ix_sb_dl'
-url
-
-
-# In[50]:
-
-
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
-
-
-# In[51]:
-
-
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-driver = webdriver.Chrome(r"C:\Users\shixinyue\Desktop\Columbia University\Tools for Analytics\web crawler\chromedriver")
-driver.get(url)
-driver.implicitly_wait(10)
-
-
-# In[53]:
-
-
-names = driver.find_elements_by_class_name('hotel-name')
-for name in names:
-    print(name.text)
-
-
-# In[57]:
 
 
 addresses = driver.find_elements_by_class_name('hotel-lm')
